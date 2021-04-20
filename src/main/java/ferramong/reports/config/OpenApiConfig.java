1918d4ab4dd8d17d0ff5e76a01b805626d0e7288
William Niemiec <williamniemiec@hotmail.com>
2021-04-20 16:32:19 -0300

    Heroku config

---
+++ src/main/java/ferramong/reports/config/OpenApiConfig.java
@@ -0,0 +1,7 @@
+package ferramong.reports.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(info = @Info(title = "FerramONG Reports API"))
+public class OpenApiConfig { }
